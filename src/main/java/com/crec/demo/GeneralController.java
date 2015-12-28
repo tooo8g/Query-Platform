93bfbab2a4c2c6fb86330334b3def028dff2aecf
zhanglei <zhangleipro@163.com>
2015-12-28 16:09:04 +0800

    v1.0

---
+++ src/main/java/com/crec/demo/GeneralController.java
@@ -0,0 +1,29 @@
+package com.crec.demo;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GeneralController {
+
+	@RequestMapping("/query")
+	public String index_jsp(Model model) {
+		model.addAttribute("key", "MVC of Spring!");
+		return "/index";
+	}
+
+	@RequestMapping("/json")
+	public void json(@RequestParam String name, HttpServletRequest request,
+			HttpServletResponse response) throws IOException {
+		System.out.println("JSON");
+		response.getWriter().print("{'name'," + name + "}");
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,23 @@
+<html>
+<head>
+<meta charset="utf-8"></meta>
+<script src="js/jquery-1.11.3.js" type="text/javascript"></script>
+<script type="text/javascript">
+	var aj = $.ajax({
+		url : '/json',// 跳转到 action  
+		type : 'get',
+		dataType : 'json',
+		success : function(data) {
+			alert(data);
+		},
+		error : function() {
+			// view("异常！");  
+			alert("异常！");
+		}
+	});
+</script>
+</head>
+<body>
+	<h2>Hello World!</h2>
+</body>
+</html>
