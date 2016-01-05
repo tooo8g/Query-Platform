dfd5fed1002b8dad04df0dfafe558913a6af34da
zhanglei <zhangleipro@163.com>
2016-01-05 10:10:39 +0800

    增加资质查询

--- src/main/java/com/crec/demo/GeneralController.java
+++ src/main/java/com/crec/demo/GeneralController.java
@@ -10,6 +10,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.platform.mongo.s1.MongoDirver;
+
 @Controller
 public class GeneralController {
 
@@ -24,6 +26,17 @@ public class GeneralController {
 			HttpServletResponse response) throws IOException {
 		System.out.println("JSON HERE!!!");
 		response.getWriter().print("{'name'," + name + "}");
+	}
+
+	@RequestMapping("/queryCert")
+	public void QueryCertification(@RequestParam String str,
+			@RequestParam int start, @RequestParam int limit,
+			HttpServletRequest request, HttpServletResponse response)
+			throws IOException {
+		System.out.println(str);
+		MongoDirver md = new MongoDirver();
+		String result = md.queryCertifications(str, start, limit);
+		response.getWriter().print(result);
 	}
 
 }
