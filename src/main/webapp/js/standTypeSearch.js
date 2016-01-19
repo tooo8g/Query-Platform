4af48b41095c0b5e6cf633f8ec409bdfa60004d8
赵宝 <zhaobao@lubanec.com>
2016-01-19 11:29:20 +0800

    标准类型查询

---
+++ src/main/webapp/js/standTypeSearch.js
@@ -0,0 +1,219 @@
+/**
+ * Created by zb on 2016/1/15.
+ */
+$(function(){
+    /*
+    * 给sts_left ul li添加一个click事件
+    * */
+    $(".sts_left ul li").on("click",function(){
+        var leftUiClassName //sts_left ul li的class值
+        leftUiClassName=$(this).attr("id")
+        if($("#sts_"+leftUiClassName).hasClass("displayNone")){
+            $("#"+leftUiClassName).removeClass("colorHui").addClass("colorRed")
+            $("#sts_"+leftUiClassName).removeClass("displayNone").addClass("displayBlock")
+        }else{
+            $("#"+leftUiClassName).removeClass("colorRed").addClass("colorHui")
+            $("#sts_"+leftUiClassName).removeClass("displayBlock").addClass("displayNone")
+            $("#sts_"+leftUiClassName+" a").removeClass("colorRed").addClass("colorHui")
+        }
+    })
+
+    $("#sts_standType a").on("click",function(){
+        $(this).siblings().removeClass("colorRed").addClass("colorHui")
+        $(this).removeClass("colorHui").addClass("colorRed")
+        var staHtml //sts_standType a的值
+        staHtml=$(this).html()
+        var standard_id="" //标准编号
+        var standard_name="" //标准名称
+        var standard_status="" //标准状态
+        var special_subject="" //专业分类
+        var startValue=0 //初始值
+        var limitValue=20 //一次取出多少条数据
+        $.ajax({
+            url:"../json/demo_standInfor.json",
+            data:{standard_group:staHtml,standard_id:standard_id,standard_name:standard_name,standard_status:standard_status,special_subject:special_subject,skip:startValue,limit:limitValue},
+            type:"post",
+            dataType : 'json',
+            success:function(data){
+               var tbodyList //保存解析的json数据
+               var count=data.count
+               var bzxx=data.bzxx
+               for(var i=0;i<bzxx.length;i++){
+                   tbodyList+="<tr>"
+                   tbodyList+="<td>"+bzxx[i].standard_id+"</td>"
+                   tbodyList+="<td title='"+bzxx[i].standard_name+"'>"+bzxx[i].standard_name+"</td>"
+                   tbodyList+="<td>"+bzxx[i].replace_id+"</td>"
+                   tbodyList+="<td>"+bzxx[i].standard_group+"</td>"
+                   tbodyList+="<td>"+bzxx[i].special_subject+"</td>"
+                   tbodyList+="<td>"+bzxx[i].standard_status+"</td>"
+                   tbodyList+="<td>"+timeStamp2String(bzxx[i].publish_date.$date)+"</td>"
+                   tbodyList+="<td>"+timeStamp2String(bzxx[i].execute_date.$date)+"</td>"
+                   tbodyList+="</tr>"
+               }
+               $(".sts_search_tbody").html(" ")
+               $(".sts_search_tbody").append(tbodyList)
+
+                var asButton=""
+                var countPages=Math.ceil(count/limitValue)
+                var PageNo  //当前页码
+                if(startValue==0){
+                    PageNo=1
+                }
+                $(".pageNo").val(PageNo)
+                var nextStartRow//下一页开始显示的编号
+                asButton+="<a>上一页</a>"
+                asButton+="<p>"+PageNo+"/"+countPages+"</p>"
+                if(countPages>1){
+                    nextStartRow=PageNo*limitValue
+                    asButton+="<a class=clickCursor onclick=goPage('"+staHtml+"','"+standard_id+"','"+standard_name+"','"+standard_status+"','"+special_subject+"','"+nextStartRow+"','"+limitValue+"','next')>下一页</a>"
+                }else{
+                    asButton+="<a>下一页</a>"
+                }
+                $(".listperAuth_button").html(" ")
+                $(".listperAuth_button").append(asButton)
+            },
+            error:function(){
+                alert("链接失败")
+            }
+        })
+    })
+
+})
+
+
+//时间格式化
+function timeStamp2String(time){
+    var datetime = new Date();
+    datetime.setTime(time);
+    var year = datetime.getFullYear();
+    var month = datetime.getMonth() + 1 < 10 ? "0" + (datetime.getMonth() + 1) : datetime.getMonth() + 1;
+    var date = datetime.getDate() < 10 ? "0" + datetime.getDate() : datetime.getDate();
+    return year + "-" + month + "-" + date;
+}
+
+/*
+* 异步提交表单
+* */
+function formButton(){
+    var startValue=0 //初始值
+    var limitValue=20 //一次取出多少条数据
+    var standard_group="" //standard_group 行业分类
+    var standard_id=$(".standard_id").val() //标准编号
+    var standard_name=$(".standard_name").val() //标准名称
+    var special_subject=$(".special_subject option:selected").val() //专业分类
+    var standard_status=$(".standard_status option:selected").val() //标准状态
+     $("#searchForm").ajaxSubmit({
+         url:"../json/demo_standInfor.json",
+         data:{standard_group:standard_group,standard_id:standard_id,standard_name:standard_name,standard_status:standard_status,special_subject:special_subject,skip:startValue,limit:limitValue},
+         type:"post",
+         dataType : 'json',
+         success:function(data){
+             var tbodyList //保存解析的json数据
+             var count=data.count
+             var bzxx=data.bzxx
+             for(var i=0;i<bzxx.length;i++){
+                 tbodyList+="<tr>"
+                 tbodyList+="<td>"+bzxx[i].standard_id+"</td>"
+                 tbodyList+="<td title='"+bzxx[i].standard_name+"'>"+bzxx[i].standard_name+"</td>"
+                 tbodyList+="<td>"+bzxx[i].replace_id+"</td>"
+                 tbodyList+="<td>"+bzxx[i].standard_group+"</td>"
+                 tbodyList+="<td>"+bzxx[i].special_subject+"</td>"
+                 tbodyList+="<td>"+bzxx[i].standard_status+"</td>"
+                 tbodyList+="<td>"+timeStamp2String(bzxx[i].publish_date.$date)+"</td>"
+                 tbodyList+="<td>"+timeStamp2String(bzxx[i].execute_date.$date)+"</td>"
+                 tbodyList+="</tr>"
+             }
+             $(".sts_search_tbody").html(" ")
+             $(".sts_search_tbody").append(tbodyList)
+
+             var asButton=""
+             var countPages=Math.ceil(count/limitValue)
+             var PageNo  //当前页码
+             if(startValue==0){
+                 PageNo=1
+             }
+             $(".pageNo").val(PageNo)
+             var nextStartRow//下一页开始显示的编号
+             asButton+="<a>上一页</a>"
+             asButton+="<p>"+PageNo+"/"+countPages+"</p>"
+             if(countPages>1){
+                 nextStartRow=PageNo*limitValue
+                 asButton+="<a class=clickCursor onclick=goPage('"+standard_group+"','"+standard_id+"','"+standard_name+"','"+standard_status+"','"+special_subject+"','"+nextStartRow+"','"+limitValue+"','next')>下一页</a>"
+             }else{
+                 asButton+="<a>下一页</a>"
+             }
+             $(".listperAuth_button").html(" ")
+             $(".listperAuth_button").append(asButton)
+         },
+         error:function(){
+             alert("链接失败")
+         }
+     })
+}
+
+//页码跳转
+function goPage(standard_group,standard_id,standard_name,standard_status,special_subject,start,limitValue,isGo){
+    $.ajax({
+        url:"../json/demo_standInfor.json",
+        data:{standard_group:standard_group,standard_id:standard_id,standard_name:standard_name,standard_status:standard_status,special_subject:special_subject,skip:start,limit:limitValue},
+        type : 'post',
+        dataType : 'json',
+        success:function(data){
+            var tbodyList //保存解析的json数据
+            var count=data.count
+            var bzxx=data.bzxx
+            for(var i=0;i<bzxx.length;i++){
+                tbodyList+="<tr>"
+                tbodyList+="<td>"+bzxx[i].standard_id+"</td>"
+                tbodyList+="<td title='"+bzxx[i].standard_name+"'>"+bzxx[i].standard_name+"</td>"
+                tbodyList+="<td>"+bzxx[i].replace_id+"</td>"
+                tbodyList+="<td>"+bzxx[i].standard_group+"</td>"
+                tbodyList+="<td>"+bzxx[i].special_subject+"</td>"
+                tbodyList+="<td>"+bzxx[i].standard_status+"</td>"
+                tbodyList+="<td>"+timeStamp2String(bzxx[i].publish_date.$date)+"</td>"
+                tbodyList+="<td>"+timeStamp2String(bzxx[i].execute_date.$date)+"</td>"
+                tbodyList+="</tr>"
+            }
+            $(".sts_search_tbody").html(" ")
+            $(".sts_search_tbody").append(tbodyList)
+
+            var asButton=""
+            var pageNo=$(".pageNo").val()  //当前页码
+            var countPages=Math.ceil(count/limitValue)
+            var noPage
+            if(isGo=="next"){
+                noPage=Number($(".pageNo").val())+1
+                if(noPage>countPages){
+                    pageNo=countPages
+                }else{
+                    pageNo=noPage
+                }
+            }
+            if(isGo=="pre"){
+                noPage=Number($(".pageNo").val())-1
+                if(noPage==0){
+                    noPage=1
+                }
+                pageNo=noPage
+            }
+            $(".pageNo").val(pageNo)
+            var preStartRow //上一页开始显示的编号
+            var nextStartRow//下一页开始显示的编号
+            if(pageNo>1){
+                preStartRow=(pageNo-1)*limitValue
+                asButton+="<a class=clickCursor onclick=goPage('"+standard_group+"','"+standard_id+"','"+standard_name+"','"+standard_status+"','"+special_subject+"','"+preStartRow+"','"+limitValue+"','pre')>上一页</a>"
+            }else{
+                asButton+="<a>上一页</a>"
+            }
+            asButton+="<p>"+pageNo+"/"+countPages+"</p>"
+            if(countPages>pageNo){
+                nextStartRow=pageNo*limitValue
+                asButton+="<a class=clickCursor onclick=goPage('"+standard_group+"','"+standard_id+"','"+standard_name+"','"+standard_status+"','"+special_subject+"','"+nextStartRow+"','"+limitValue+"','next')>下一页</a>"
+            }else{
+                asButton+="<a>下一页</a>"
+            }
+            $(".listperAuth_button").html(" ")
+            $(".listperAuth_button").append(asButton)
+        }
+    })
+}
---
+++ src/main/webapp/views/standTypeSearch.jsp
@@ -0,0 +1,109 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>标准类型查询</title>
+    <script type="text/javascript" src="../js/jquery-1.11.3.js"></script>
+    <script type="text/javascript" src="../js/jquery-form.js"></script>
+    <script type="text/javascript" src="../js/standTypeSearch.js"></script>
+    <link rel="stylesheet" type="text/css" href="../css/standTypeSearch.css">
+</head>
+<body>
+    <input type="hidden" class="pageNo" value="">
+    <div class="sts">
+        <div class="sts_title">
+            <p>当前位置：</p>
+            <p>技术标准查询>></p>
+            <p>标准类型查询</p>
+        </div>
+        <div class="sts_left">
+            <p>目录</p>
+            <ul>
+                <li >国家标准</li>
+                <li id="standType" class="colorHui">行业标准</li>
+                <div id="sts_standType" class="displayNone">
+                    <a class="colorHui">铁路行业标准</a>
+                    <a class="colorHui">电力行业标准</a>
+                    <a class="colorHui">冶金行业标准</a>
+                </div>
+                <li>标准型技术文件</li>
+                <li>铁道计量检定规程</li>
+            </ul>
+        </div>
+        <div class="sts_right">
+            <div class="sts_right_top">
+                <form id="searchForm">
+                    <div class="sts_search_formList">
+                        <div class="sts_search_l">
+                            <div>
+                                <span>标准编号</span>
+                                <input type="text" name="standard_id" class="standard_id" value="">
+                            </div>
+                            <div>
+                                <span>文件状态</span>
+                                <select class="standard_status">
+                                    <option value="现行" selected>现行</option>
+                                    <option value="作废">作废</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="sts_search_r">
+                            <div>
+                                <span>标准名称</span>
+                                <input type="text" name="standard_name" class="standard_name" value="">
+                            </div>
+                            <div>
+                                <span>专业分类</span>
+                                <select class="special_subject">
+                                    <option selected value="">全部</option>
+                                    <option value="基础与综合">基础与综合</option>
+                                    <option value="工务工程">工务工程</option>
+                                    <option value="机车车辆综合">机车车辆综合</option>
+                                    <option value="牵引供电">牵引供电</option>
+                                    <option value="铁道机车">铁道机车</option>
+                                    <option value="铁道车辆">铁道车辆</option>
+                                    <option value="铁路通信">铁路通信</option>
+                                    <option value="铁路信号">铁路信号</option>
+                                    <option value="铁路运输">铁路运输</option>
+                                    <option value="通信信号综合">通信信号综合</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="sts_search_sub">
+                       <input type="button"  value="查询" id="form_button" onclick="formButton()">
+                       <input type="reset"  value="清除" id="reset_submit">
+                    </div>
+                </form>
+            </div>
+            <div class="sts_right_bottom">
+                <div class="sts_table_title">
+                    找到100条记录
+                </div>
+                <div class="sts_table">
+                    <table class="sts_search_table">
+                        <thead class="sts_search_thead">
+                        <tr>
+                            <td>标准编号</td>
+                            <td width="300">标准名称</td>
+                            <td>代替标准号</td>
+                            <td>标准类别</td>
+                            <td>专业分类</td>
+                            <td>标准状态</td>
+                            <td>发布日期</td>
+                            <td>实施日期</td>
+                        </tr>
+                        </thead>
+                        <tbody class="sts_search_tbody">
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div class="listperAuth_button">
+
+            </div>
+        </div>
+    </div>
+</body>
+</html>
