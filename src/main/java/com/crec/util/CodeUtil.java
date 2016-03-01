50ec0814f896312e153fc708ace877e2429e626b
zhanglei <zhangleipro@163.com>
2016-03-01 17:22:35 +0800

    增加编码相关方法

---
+++ src/main/java/com/crec/util/CodeUtil.java
@@ -0,0 +1,30 @@
+package com.crec.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.crec.bean.Code;
+
+public class CodeUtil {
+
+	/**
+	 * 
+	 * @auth zhanglei
+	 * @param code
+	 *            编码原始数据
+	 * @param num
+	 *            生成数量
+	 * @return
+	 */
+	public static List<Code> codec(Code code, int num) {
+		List<Code> codes = new ArrayList<Code>();
+		for (int i = 0; i < num; i++) {
+			Code c = (Code) code.clone();
+			c.setCode(i + "");
+			c.setInner_id(i + "");
+			codes.add(c);
+		}
+
+		return codes;
+	}
+}
