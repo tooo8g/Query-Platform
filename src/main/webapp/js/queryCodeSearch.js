661fa16fa47d2079a57880a8c3702205c94ba872
张友彬 <zhangyoubin@lubanec.com>
2016-03-02 15:26:37 +0800

    后台代码提交

--- src/main/webapp/js/queryCodeSearch.js
+++ src/main/webapp/js/queryCodeSearch.js
@@ -87,7 +87,7 @@ function  formButton(){
     var tbodyList=""
     var bzNum
     $.ajax({
-        url:"../product",
+        url:"../produc",
         data:{company_name:company_name,product_identify:product_identify,product_name:product_name,specification:specification,start:startValue,limit:limitValue},
         type:"post",
         dataType:"json",
