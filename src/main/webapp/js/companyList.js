ca83e1948925efcbb1f150aba0db2486196d3bad
赵宝 <zhaobao@lubanec.com>
2016-03-29 10:46:24 +0800

    公司列表 新增公司

---
+++ src/main/webapp/js/companyList.js
@@ -0,0 +1,221 @@
+/**
+ * Created by zb on 2016/3/28.
+ */
+$(function(){
+    /*页面刚点开调用此方法*/
+    var startValue=0 //初始值
+    var limitValue=10 //一次取出多少条数据
+    var com_name="" //公司名称
+    var org_code="" //组织机构代码
+    var count="" //总数
+    var companyList="" //保存data信息
+    var tbodyList=""
+    var bzNum
+    $.ajax({
+        url:"../json/demo_companyList.json",
+        type:"post",
+        data:{com_name:com_name,org_code:org_code,start:startValue,limit:limitValue},
+        dataType:"json",
+        success:function(data){
+            count=data.count
+            companyList=data.companyList
+            for(var i=0;i<companyList.length;i++){
+                bzNum=Number(startValue)+i+1
+                tbodyList+="<tr>"
+                tbodyList+="<td>"+bzNum+"</td>"
+                tbodyList+="<td>"+companyList[i].com_name+"</td>"
+                tbodyList+="<td>"+companyList[i].org_code+"</td>"
+                tbodyList+="<td>"+timeStamp2String(companyList[i].add_time.$date)+"</td>"
+                tbodyList+="<td>"+companyList[i].com_addr+"</td>"
+                tbodyList+="<td>"+companyList[i].con_person+"</td>"
+                tbodyList+="<td>"+companyList[i].con_way+"</td>"
+                tbodyList+="</tr>"
+            }
+            $(".cpl_bottom_tbody").html("")
+            $(".cpl_bottom_tbody").append(tbodyList)
+
+            if(count>0) {
+                var asButton = ""
+                var countPages = Math.ceil(count / limitValue)
+                var PageNo  //当前页码
+                if (startValue == 0) {
+                    PageNo = 1
+                }
+                $(".pageNo").val(PageNo)
+                var nextStartRow//下一页开始显示的编号
+                asButton += "<a><img src='../images/sts_4.png'></a>"
+                asButton += "<p>" + PageNo + "/" + countPages + "</p>"
+                if (countPages > 1) {
+                    nextStartRow = PageNo * limitValue
+                    asButton += "<a class=clickCursor onclick=goPage('" + com_name + "','" + org_code + "','" + nextStartRow + "','" + limitValue + "','next')><img src='../images/sts_5.png'></a>"
+                } else {
+                    asButton += "<a><img src='"+ctx+"/images/sts_5.png'></a>"
+                }
+                $(".listperAuth_button").html(" ")
+                $(".listperAuth_button").append(asButton)
+            }
+        },
+        error:function(){
+            alert("链接失败")
+        }
+    })
+})
+/*查询*/
+function formButton(){
+    var startValue=0 //初始值
+    var limitValue=10 //一次取出多少条数据
+    var com_name=$(".top_com_name").val() //公司名称
+    var org_code=$(".top_org_code").val() //组织机构代码
+    var count="" //总数
+    var companyList="" //保存data信息
+    var tbodyList=""
+    var bzNum
+    $.ajax({
+        url:"../json/demo_companyList.json",
+        type:"post",
+        data:{com_name:com_name,org_code:org_code,start:startValue,limit:limitValue},
+        dataType:"json",
+        success:function(data){
+            count=data.count
+            companyList=data.companyList
+            for(var i=0;i<companyList.length;i++){
+                bzNum=Number(startValue)+i+1
+                tbodyList+="<tr>"
+                tbodyList+="<td>"+bzNum+"</td>"
+                tbodyList+="<td>"+companyList[i].com_name+"</td>"
+                tbodyList+="<td>"+companyList[i].org_code+"</td>"
+                tbodyList+="<td>"+timeStamp2String(companyList[i].add_time.$date)+"</td>"
+                tbodyList+="<td>"+companyList[i].com_addr+"</td>"
+                tbodyList+="<td>"+companyList[i].con_person+"</td>"
+                tbodyList+="<td>"+companyList[i].con_way+"</td>"
+                tbodyList+="</tr>"
+            }
+            $(".cpl_bottom_tbody").html("")
+            $(".cpl_bottom_tbody").append(tbodyList)
+
+            if(count>0) {
+                var asButton = ""
+                var countPages = Math.ceil(count / limitValue)
+                var PageNo  //当前页码
+                if (startValue == 0) {
+                    PageNo = 1
+                }
+                $(".pageNo").val(PageNo)
+                var nextStartRow//下一页开始显示的编号
+                asButton += "<a><img src='"+ctx+"/images/sts_4.png'></a>"
+                asButton += "<p>" + PageNo + "/" + countPages + "</p>"
+                if (countPages > 1) {
+                    nextStartRow = PageNo * limitValue
+                    asButton += "<a class=clickCursor onclick=goPage('" + com_name + "','" + org_code + "','" + nextStartRow + "','" + limitValue + "','next')><img src='../images/sts_5.png'></a>"
+                } else {
+                    asButton += "<a><img src='"+ctx+"/images/sts_5.png'></a>"
+                }
+                $(".listperAuth_button").html(" ")
+                $(".listperAuth_button").append(asButton)
+            }
+        },
+        error:function(){
+            alert("链接失败")
+        }
+    })
+}
+/*清除*/
+function resetSubmit(){
+    $("#cpl_top_form")[0].reset()
+}
+/*新增公司
+* 点击此方法，弹出悬浮框
+* */
+function companyCreat(){
+   $(".cpl_create").removeClass("displayNo").addClass("displayBlock")
+}
+
+//页码跳转
+function goPage(com_name,org_code,startValue,limitValue,isGo){
+    $.ajax({
+        url:"../json/demo_companyList.json",
+        data:{com_name:com_name,org_code:org_code,start:startValue,limit:limitValue},
+        type : 'post',
+        dataType : 'json',
+        success:function(data){
+            var count="" //总数
+            var companyList="" //保存data信息
+            var tbodyList=""
+            var bzNum
+            count=data.count
+            companyList=data.companyList
+            for(var i=0;i<companyList.length;i++){
+                bzNum=Number(startValue)+i+1
+                tbodyList+="<tr>"
+                tbodyList+="<td>"+bzNum+"</td>"
+                tbodyList+="<td>"+companyList[i].com_name+"</td>"
+                tbodyList+="<td>"+companyList[i].org_code+"</td>"
+                tbodyList+="<td>"+timeStamp2String(companyList[i].add_time.$date)+"</td>"
+                tbodyList+="<td>"+companyList[i].com_addr+"</td>"
+                tbodyList+="<td>"+companyList[i].con_person+"</td>"
+                tbodyList+="<td>"+companyList[i].con_way+"</td>"
+                tbodyList+="</tr>"
+            }
+            $(".cpl_bottom_tbody").html("")
+            $(".cpl_bottom_tbody").append(tbodyList)
+
+
+            var asButton=""
+            var pageNo=$(".pageNo").val()  //当前页码
+            var countPages=Math.ceil(count/limitValue)
+            var noPage
+            if(isGo=="next"){
+                noPage=Number($(".pageNo").val())+1
+                if(noPage>countPages){
+                    pageNo=countPages
+                }else{
+                    pageNo=noPage
+                }
+            }
+            if(isGo=="pre"){
+                noPage=Number($(".pageNo").val())-1
+                if(noPage==0){
+                    noPage=1
+                }
+                pageNo=noPage
+            }
+            $(".pageNo").val(pageNo)
+            var preStartRow //上一页开始显示的编号
+            var nextStartRow//下一页开始显示的编号
+            if(pageNo>1){
+                preStartRow=(pageNo-2)*limitValue
+                asButton+="<a class=clickCursor onclick=goPage('"+com_name+"','"+org_code+"','"+preStartRow+"','"+limitValue+"','pre')><img src='../images/sts_4.png'></a>"
+            }else{
+                asButton+="<a><img src='"+ctx+"/images/sts_4.png'></a>"
+            }
+            asButton+="<p>"+pageNo+"/"+countPages+"</p>"
+            if(countPages>pageNo){
+                nextStartRow=pageNo*limitValue
+                asButton+="<a class=clickCursor onclick=goPage('"+com_name+"','"+org_code+"','"+nextStartRow+"','"+limitValue+"','next')><img src='../images/sts_5.png'></a>"
+            }else{
+                asButton+="<a><img src='"+ctx+"/images/sts_5.png'></a>"
+            }
+            $(".listperAuth_button").html(" ")
+            $(".listperAuth_button").append(asButton)
+        }
+    })
+}
+
+/*关闭新增页面*/
+function closeCreate(){
+    $(".cpl_create").removeClass("displayBlock").addClass("displayNo")
+}
+/*保存公司*/
+function creat_button(){
+     var option={
+         url:"companyList.html",
+         type:"post",
+         success:function(data){
+             if(data=="true"){
+                 formButton()
+                 closeCreate()
+             }
+         }
+     }
+    $("#create_cpl_form").ajaxSubmit(option)
+}
---
+++ src/main/webapp/views/companyList.jsp
@@ -0,0 +1,96 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <jsp:include page="resource.jsp"/>
+    <title>公司列表</title>
+    <script type="text/javascript" src="${ctx}/js/jquery-1.11.3.js"></script>
+    <script type="text/javascript" src="${ctx}/js/companyList.js"></script>
+    <script type="text/javascript" src="${ctx}/js/transTime.js"></script>
+    <script type="text/javascript" src="${ctx}/js/jquery-form.js"></script>
+    <link rel="stylesheet" type="text/css" href="${ctx}/css/companyList.css">
+</head>
+<body>
+<jsp:include page="head_code.jsp"/>
+<div class="cpl">
+  <input type="hidden" class="pageNo" value="">
+  <div class="cpl_top">
+    <form class="cpl_top_form" id="cpl_top_form">
+      <div class="cpl_top_com_name">
+        <label>公司名称</label>
+        <input type="text" class="top_com_name" value="">
+      </div>
+      <div class="cpl_top_org_code">
+        <label>组织机构代码</label>
+        <input type="text" class="top_org_code" value="">
+      </div>
+      <div class="cpl_top_click">
+        <a type="reset" id="reset_submit" onclick="resetSubmit()">清除</a>
+        <a type="button" id="form_button" onclick="formButton()">查询</a>
+        <a href="javaScript:;"  class="companyCreat" onclick="companyCreat()">新增</a>
+      </div>
+    </form>
+  </div>
+  <div class="cpl_bottom">
+    <table class="cpl_bottom_table">
+      <thead class="cpl_bottom_thead">
+      <tr>
+        <td width="73"></td>
+        <td width="200">公司名称</td>
+        <td>组织机构代码</td>
+        <td>注册时间</td>
+        <td width="300">公司地址</td>
+        <td>联系人</td>
+        <td>联系方式</td>
+      </tr>
+      </thead>
+      <tbody class="cpl_bottom_tbody">
+
+      </tbody>
+    </table>
+  </div>
+  <div class="listperAuth_button">
+
+  </div>
+</div>
+<div class="cpl_create displayNo">
+  <div class="cplCreate">
+    <div class="create_cpl">
+      <div class="create_cpl_title">
+        <p>新增公司</p>
+        <a href="javaScript:;" class="create_cpl_title_a" onclick="closeCreate()"><img src="${ctx}/images/qws_1.png" ></a>
+      </div>
+      <form id="create_cpl_form">
+        <div class="create_cpl_content">
+          <div>
+            <label>公司名称</label>
+            <input type="text" class="com_name" name="com_name" value="">
+          </div>
+          <div>
+            <label>组织机构代码</label>
+            <input type="text" class="org_code" name="org_code" value="">
+          </div>
+          <div>
+            <label>公司地址</label>
+            <input type="text" class="com_addr" name="com_addr" value="">
+          </div>
+          <div>
+            <label>联系人</label>
+            <input type="text" class="con_person" name="con_person" value="">
+          </div>
+          <div>
+            <label>联系方式</label>
+            <input type="text" class="con_method" name="con_method" value="">
+          </div>
+          <div class="creat_button">
+            <a href="javaScript:;" onclick="creat_button()">保存</a>
+          </div>
+        </div>
+
+      </form>
+
+    </div>
+  </div>
+</div>
+<jsp:include page="foot.jsp"/>
+</body>
+</html>
