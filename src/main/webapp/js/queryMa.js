f7fa8ffaeeb06068cb6ca09ca6bfaa6af544cb5e
赵宝 <zhaobao@lubanec.com>
2017-01-09 14:53:08 +0800

    标准名称管理

--- src/main/webapp/js/queryMa.js
+++ src/main/webapp/js/queryMa.js
@@ -7,19 +7,6 @@ $(function () {
     })
 })
 
-// 右侧加载相应的页面
-function jumpPage(str) {
-    $(".nm-l a").removeClass("aClick").addClass("aNoClick")
-    if(str=='n'){
-        $(".a_n").removeClass("aNoClick").addClass("aClick")
-    }else if(str=='m'){
-        $(".a_m").removeClass("aNoClick").addClass("aClick")
-    }else if(str=='nm'){
-        $(".a_nm").removeClass("aNoClick").addClass("aClick")
-    }else{
-
-    }
-}
 
 //导入按钮，用html5的FileReader方法 导入标准名称
 function importMa(){
