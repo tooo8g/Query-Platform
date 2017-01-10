c62e25c1c6b8cd5cafb6457b4f737764027d4a52
赵宝 <zhaobao@lubanec.com>
2017-01-10 10:47:50 +0800

    New changelist

--- src/main/webapp/js/queryWordLeft.js
+++ src/main/webapp/js/queryWordLeft.js
@@ -4,10 +4,13 @@ function jumpPage(str) {
     $(".nm-l a").removeClass("aClick").addClass("aNoClick")
     if(str=='n'){
         $(".a_n").removeClass("aNoClick").addClass("aClick")
+        window.location.href=ctx+"/queryNoMa.jsp"
     }else if(str=='m'){
         $(".a_m").removeClass("aNoClick").addClass("aClick")
+        window.location.href=ctx+"/queryMa.jsp"
     }else if(str=='nm'){
         $(".a_nm").removeClass("aNoClick").addClass("aClick")
+        window.location.href=ctx+"/queryDataAss.jsp"
     }else{
 
     }
